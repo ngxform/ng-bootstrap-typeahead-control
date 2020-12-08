da19b2ae88bf85a3fc810261ea9d47b35288cfcc
hieupv <hieupv@codersvn.com>
2020-12-08 13:34:14 +0700

    fix issue NgbTypeaheadConfig is not provided

--- src/lib/ng-bootstrap-typeahead.module.ts
+++ src/lib/ng-bootstrap-typeahead.module.ts
@@ -6,11 +6,14 @@ import { CommonModule } from '@angular/common';
 import { NgBootstrapTypeaheadExtendComponent } from './controls/extend/extend.component';
 import { NgbHighlight } from './typeahead/highlight';
 import { NgbTypeaheadModule } from './typeahead/typeahead.module';
+import { NgbTypeaheadConfig } from './typeahead/typeahead-config';
+import { Live } from './util/accessibility/live';
 
 @NgModule({
   declarations: [NgBootstrapTypeaheadComponent, NgBootstrapTypeaheadExtendComponent],
   imports: [NgbTypeaheadModule, CommonModule, FormsModule, ReactiveFormsModule, NgxFormModule],
   exports: [NgbHighlight],
+  providers: [NgbTypeaheadConfig, Live],
   entryComponents: [NgBootstrapTypeaheadComponent, NgBootstrapTypeaheadExtendComponent]
 })
 export class NgxFormTypeaheadModule {}
